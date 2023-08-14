00a0465d52ed4dcba1a1f101877206b53f2d7c7a
alek <wait82985@gmail.com>
2023-08-14 11:44:30 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,19 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        //TODO
-        return 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(priorityQueue.size() < k){
+                priorityQueue.add(nums[i]);
+            }else {
+                if(nums[i] > priorityQueue.peek()){
+                    priorityQueue.poll();
+                    priorityQueue.add(nums[i]);
+                }
+            }
+        }
+        return priorityQueue.poll();
     }
 }
